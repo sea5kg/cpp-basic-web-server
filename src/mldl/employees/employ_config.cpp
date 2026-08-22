e20443c8e2d2a90e8622bb92d91da68366703aee
Evgenii Sopov <mrseakg@gmail.com>
2026-08-22 15:18:25 +0700

    Copied code from ctf01d for extracting files html

--- src/mldl/employees/employ_config.cpp
+++ src/mldl/employees/employ_config.cpp
@@ -34,7 +34,11 @@
 #include "mldl/include/config.h"
 #include "mldl/include/webhooks.h"
 #include "mldl/objects/mldl_webhook_git_repo_update.h"
+#include "third_party/smallsha1/smallsha1.h"
+#include <fstream>
+#include <nlohmann/json.hpp>
 #include <sea5kg_logger.h>
+// #include <sys/stat.h>
 #include <wsjcpp_core.h>
 #include <wsjcpp_employees.h>
 #include <wsjcpp_yaml.h>
@@ -58,6 +62,10 @@ public:
 
 private:
   bool read_mapping_host_and_path_folders(WsjcppYaml &yaml);
+  nlohmann::json load_files_sha1();
+  void save_files_sha1(nlohmann::json &files);
+  void update_data_html(nlohmann::json &files);
+  void update_files_in_data();
 
   std::string TAG;
   std::string m_sConfigDir;
@@ -89,6 +97,9 @@ bool employ_config::deinit(const std::string &sName, bool bSilent) {
 void employ_config::set_data_dir(const std::string data_dir) {
   sea5kg::log::info(TAG, "setDataDir: " + data_dir);
   m_sConfigDir = data_dir;
+
+  this->update_files_in_data();
+
   std::string sConfigFile = data_dir + "/config.yml";
   if (!wsjcpp::file_exists(sConfigFile)) {
     sea5kg::log::critical(TAG, "File not found " + sConfigFile);
@@ -158,7 +169,8 @@ bool employ_config::read_mapping_host_and_path_folders(WsjcppYaml &yaml) {
 
   // hardcoded root page for localhost
   m_mapping["localhost"] = std::map<std::string, std::string>();
-  m_mapping["localhost"]["/"] = wsjcpp::normalize_filepath(m_sConfigDir + "/html/");;
+  m_mapping["localhost"]["/"] = wsjcpp::normalize_filepath(m_sConfigDir + "/html/");
+  ;
   // m_mapping["localhost"]["/admin/"] = admin_path;
 
   std::string control_panel_path = wsjcpp::normalize_filepath(m_sConfigDir + "/html/control-panel");
@@ -209,4 +221,154 @@ bool employ_config::read_mapping_host_and_path_folders(WsjcppYaml &yaml) {
     return true;
   }
   return false;
+}
+
+nlohmann::json employ_config::load_files_sha1() {
+  nlohmann::json files_sha1;
+  if (wsjcpp::file_exists(m_sConfigDir + "/files_sha1.json")) {
+    std::ifstream ifs(m_sConfigDir + "/files_sha1.json");
+    files_sha1 = nlohmann::json::parse(ifs);
+  }
+  return files_sha1;
+}
+
+void employ_config::save_files_sha1(nlohmann::json &files) {
+  std::ofstream output(m_sConfigDir + "/files_sha1.json");
+  output << std::setw(2) << files << std::endl;
+}
+
+std::string sha1_by_data(const char *data, int len) {
+  char hexstring[41]; // 40 chars + a zero
+  std::memset(hexstring, 0, sizeof hexstring);
+
+  unsigned char hash[20];
+  sha1::calc(data, len, hash);
+  sha1::toHexString(hash, hexstring);
+  return std::string(hexstring);
+}
+
+std::string sha1_by_file(const std::string &sFilename) {
+  std::ifstream f(sFilename, std::ifstream::binary);
+  if (!f) {
+    return "Could not open file";
+  }
+  // get length of file:
+  f.seekg(0, f.end);
+  int nBufferSize = f.tellg();
+  f.seekg(0, f.beg);
+  char *pBuffer = new char[nBufferSize];
+  // read data as a block:
+  f.read(pBuffer, nBufferSize);
+  if (!f) {
+    delete[] pBuffer;
+    // f.close();
+    sea5kg::log::critical("sha1_by_file", "Could not read file. Only " + std::to_string(f.gcount()) + " could be read");
+    return "";
+  }
+  f.close();
+  char hexstring[41]; // 40 chars + a zero
+  std::memset(hexstring, 0, sizeof hexstring);
+  unsigned char hash[20];
+  sha1::calc(pBuffer, nBufferSize, hash);
+  sha1::toHexString(hash, hexstring);
+  delete[] pBuffer;
+  return std::string(hexstring);
+}
+
+void employ_config::update_data_html(nlohmann::json &previous_files_sha1) {
+  sea5kg::log::warning(TAG, "Updating files in data/html");
+  if (!wsjcpp::dir_exists(m_sConfigDir + "/html")) {
+    WsjcppCore::makeDir(m_sConfigDir + "/html");
+  }
+
+  const std::vector<WsjcppResourceFile *> &vFiles = WsjcppResourcesManager::list();
+  for (int i = 0; i < vFiles.size(); i++) {
+    std::string source_filepath = vFiles[i]->getFilename();
+    if (source_filepath.rfind("./data/html/", 0) != 0) {
+      continue;
+    }
+    // remove base folder
+    std::vector<std::string> vPath = wsjcpp::split(source_filepath, "/");
+    vPath.erase(vPath.begin(), vPath.begin() + 3);
+    std::string target_filepath = wsjcpp::join(vPath, "/");
+    target_filepath = wsjcpp::normalize_filepath(m_sConfigDir + "/html/" + target_filepath);
+
+    // prepare folders
+    if (!wsjcpp::file_exists(target_filepath)) {
+      std::string dirpath = wsjcpp::normalize_filepath(m_sConfigDir + "/html/");
+      for (int p = 0; p < vPath.size() - 1; p++) {
+        dirpath = wsjcpp::normalize_filepath(dirpath + "/" + vPath[p]);
+        if (!wsjcpp::dir_exists(dirpath)) {
+          if (!WsjcppCore::makeDir(dirpath)) {
+            std::cout << "ERROR. Could not create: " << dirpath << std::endl;
+            continue;
+          }
+        }
+      }
+    }
+
+    std::string previous_sha1 = "";
+    if (previous_files_sha1.contains(source_filepath)) {
+      previous_sha1 = previous_files_sha1[source_filepath];
+    }
+
+    if (wsjcpp::file_exists(target_filepath) && previous_sha1 != "") {
+      if (previous_sha1 != sha1_by_file(target_filepath)) {
+        // Skip. file has changes by user. Skip.
+        std::cout << "Warning. Could not override file, because has changes: " << target_filepath << std::endl;
+        continue;
+      }
+    }
+
+    std::string new_sha1 = sha1_by_data(vFiles[i]->getBuffer(), vFiles[i]->getBufferSize());
+    if (wsjcpp::file_exists(target_filepath) && new_sha1 == previous_sha1) {
+      // Skip. file has same content
+      continue;
+    }
+
+    if (!WsjcppCore::writeFile(target_filepath, vFiles[i]->getBuffer(), vFiles[i]->getBufferSize())) {
+      std::cout << "ERROR. Could not write/override file. " << std::endl;
+      continue;
+    }
+
+    std::cout << "Successfully created/updated file: " << target_filepath << std::endl;
+    std::string err;
+    if (!WsjcppCore::setFilePermissions(target_filepath, WsjcppFilePermissions(0x644), err)) {
+      sea5kg::log::critical(TAG, err);
+    }
+    previous_files_sha1[source_filepath] = new_sha1;
+  }
+}
+
+void employ_config::update_files_in_data() {
+  std::string sError;
+  if (!wsjcpp::dir_exists(m_sConfigDir + "/logs")) {
+    WsjcppCore::makeDir(m_sConfigDir + "/logs");
+    if (!WsjcppCore::setFilePermissions(m_sConfigDir + "/logs", WsjcppFilePermissions(0x755), sError)) {
+      sea5kg::log::critical(TAG, sError);
+    }
+  }
+
+  nlohmann::json previous_files_sha1 = load_files_sha1();
+
+  if (!wsjcpp::file_exists(m_sConfigDir + "/config.yml")) {
+    sea5kg::log::warning(TAG, "Extracting config.yml and files");
+
+    const std::vector<WsjcppResourceFile *> &vFiles = WsjcppResourcesManager::list();
+    std::vector<std::string> vExecutableFiles;
+    // for (int i = 0; i < vFiles.size(); i++) {
+    //   std::string filepath = vFiles[i]->getFilename();
+    // }
+
+    WsjcppResourceFile *pConfigYml = WsjcppResourcesManager::get("./data/config.yml");
+    std::string sNewFilepath = wsjcpp::normalize_filepath(m_sConfigDir + "/config.yml");
+    if (!WsjcppCore::writeFile(sNewFilepath, pConfigYml->getBuffer(), pConfigYml->getBufferSize())) {
+      std::cout << "ERROR. Could not write file. " << std::endl;
+    } else {
+      std::cout << "Successfully created file. " << std::endl;
+    }
+  }
+
+  update_data_html(previous_files_sha1);
+  save_files_sha1(previous_files_sha1);
 }
